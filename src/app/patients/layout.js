0d844b0e1881b2cfba5f75c1c642b82bbf245983
Selkyn Kimery <sylfra09@gmail.com>
2025-01-22 00:59:29 +0100

    restore mainlayout

--- src/app/patients/layout.js
+++ src/app/patients/layout.js
@@ -1,6 +1,5 @@
 import MainLayout from "../components/MainLayout";
 
 export default function PatientsLayout({ children }) {
-  return <>
-    {children}</>;
+  return <MainLayout>{children}</MainLayout>;
 }
